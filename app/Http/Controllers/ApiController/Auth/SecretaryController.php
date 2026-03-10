25ef0c69d72fb03a7d764997baac73ce7252dc8e
e-mandy <andymfrd02@gmail.com>
2026-03-10 15:07:05 +0000

    feat(register-secretary): Set up of the register controller for the secretary registration

--- app/Http/Controllers/ApiController/Auth/SecretaryController.php
+++ app/Http/Controllers/ApiController/Auth/SecretaryController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers\ApiController\Auth;
 
+use App\DTOs\Auth\RegisterSecretaryDTO;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterSecretaryRequest;
 use App\Services\AuthService;
+use Exception;
 
 class SecretaryController extends Controller
 {
     public function __construct(
         public AuthService $service
     ){}
+
+    public function register(RegisterSecretaryRequest $request){
+        $data = RegisterSecretaryDTO::fromRequest($request);
+
+        try{
+            $response = $this->service->registerAsSecretary($data);
+
+            return response()->json([
+                "type" => "Sécrétariat Login",
+                "message" => "Utilisateur connecté avec succès",
+                "data" => $response
+            ], 200);
+        }catch(Exception $e){
+            return response()->json([
+                "message" => $e->getMessage()
+            ], 400);
+        }
+    }
 }
