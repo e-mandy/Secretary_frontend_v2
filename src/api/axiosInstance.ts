9d18072fb20b182b55c9b00dc299dec57d24276e
e-mandy <andymfrd02@gmail.com>
2026-03-13 02:33:25 +0000

    feat(axios-instance): Set up of the two axios instances

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: import.meta.env.BACKEND_URL,
+    headers: { 'Accept': "application/json"}
+});
+
+export const  axiosPrivateInstance = axios.create({
+    baseURL: import.meta.env.BACKEND_URL,
+    withCredentials: true,
+    headers: { 
+        'Content-Type': "application/json",
+        'Accept': "application/json"
+    }
+});
