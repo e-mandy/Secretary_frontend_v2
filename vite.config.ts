0594d6b68e2a3997c64abbb1d63b22808aa1ca37
e-mandy <andymfrd02@gmail.com>
2026-04-12 16:14:54 +0100

    chore: Added the vercel.json for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8000',
-                changeOrigin: true,
-            }
-        }
+    proxy: {
+      "/api": {
+        target: "https://secretary-backend.free.laravel.cloud",
+        changeOrigin: true,
+      },
     },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
-})
+});
