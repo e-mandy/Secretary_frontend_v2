2a526a8cdf43542a232c381768dfd9e74c767e4c
e-mandy <andymfrd02@gmail.com>
2026-07-21 00:32:55 +0100

    feat(defense-reports): Started integreting research of defense report

--- src/features/defense_reports/components/DefenseTable.tsx
+++ src/features/defense_reports/components/DefenseTable.tsx
@@ -1,14 +1,41 @@
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { useDefense } from "../api/useDefense";
+import { useState, type ChangeEvent } from "react";
+import type { DefenseReportType } from "../schemas/defense_report.schema";
 
 const DefenseTable = () => {
   const {
     fetchAllDefense: { data },
   } = useDefense();
+
+  const [query, setQuery] = useState("");
+
+  const handleDefenseReportSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value.trim().toLowerCase());
+  };
+
+  const filteredDefenseReports = data
+    ? data.filter((defense: DefenseReportType) => {
+        defense.owner.toLowerCase().includes(query);
+      })
+    : [];
+
   return (
     <div className="mt-10">
-      <DataTable columns={columns} data={data ?? []} />
+      <div>
+        <span>Recherche un PV de Soutenance</span>
+        <div className="border-2 w-100 py-1 px-3 rounded mb-3">
+          <input
+            type="text"
+            className="w-full outline-none"
+            placeholder="Rechercher..."
+            onChange={handleDefenseReportSearch}
+          />
+        </div>
+      </div>
+
+      <DataTable columns={columns} data={data} />
     </div>
   );
 };
